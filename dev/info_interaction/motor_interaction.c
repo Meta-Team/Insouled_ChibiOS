3866e7d8e34fadb22fb949ef857f0880557ed3bb
Lan Tian <xuyh0120@outlook.com>
2018-05-27 23:56:59 +0800

    Minor changes

--- dev/info_interaction/motor_interaction.c
+++ dev/info_interaction/motor_interaction.c
@@ -17,7 +17,7 @@ static const CANConfig cancfg = {
 void process_chassis_feedback(CANRxFrame* rxmsg) {
     int motor_id = rxmsg->SID - 0x201;
     uint16_t report_angle = (rxmsg->data8[0] << 8 | rxmsg->data8[1]);
-    chassis.motor[motor_id].actual_angle = report_angle / 8192;
+    chassis.motor[motor_id].actual_angle = report_angle * 360 / 8192;
     chassis.motor[motor_id].actual_rpm = (rxmsg->data8[2] << 8 | rxmsg->data8[3]);
     chassis.motor[motor_id].actual_current = (rxmsg->data8[4] << 8 | rxmsg->data8[5]);
     chassis.motor[motor_id].actual_temperature = rxmsg->data8[6];
--- dev/info_interaction/motor_interaction.h
+++ dev/info_interaction/motor_interaction.h
@@ -12,9 +12,12 @@
 
 #include "remote.h"
 
-void motor_can_init(void);
+void process_chassis_feedback(CANRxFrame* rxmsg);
+void process_gimbal_feedback(CANRxFrame* rxmsg);
 
 void send_chassis_currents(void);
 void send_gimbal_currents(void);
+
+void motor_can_init(void);
 
 #endif //INSOULED_CHIBIOS_MOTOR_INTERACTION_H
